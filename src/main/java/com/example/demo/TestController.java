da790548c179cef3a8df915c7b5130441bb31eba
hyohee0613 <gygml9@gmail.com>
2025-07-31 11:28:01 +0900

    í초기설정

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("hi")
+	public String Test() {
+
+		return "내가 만든 서버";
+
+	}
+
+
+}
